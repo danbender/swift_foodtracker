a66c2a221abd86d40bfaffc2a50e7f66c719b650
Dan Bender <hello@danbender.net>
2015-10-30 06:02:24 +0100

    Add DataController, import UIKit, import CoreData, class func WIP jsonAsUSDAIdAndNameSearchResults

---
+++ FoodTrack/DataController.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+import UIKit
+import CoreData
+
+
+class DataController {
+    
+    class func jsonAsUSDAIdAndNameSearchResults (json : NSDictionary) -> [ (name:String, idValue:String) ] {
+        
+    }
+    
+    
+}
